e3490293c433816a5f6fb4dafc884ebef2420502
Jenny Patel <pateljenny555@gmail.com>
2023-04-16 12:00:22 -0700

    First Commit

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+        <h2>404</h2>
+        <p>Sorry, Requested page does not exists.</p>
+    </div>
+  )
+}
+
+export default NotFound;
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo deleniti laborum labore? Consectetur sunt ipsa eligendi quos, ratione enim aut, fuga id repellat cum soluta iste doloribus labore explicabo similique.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo deleniti laborum labore? Consectetur sunt ipsa eligendi quos, ratione enim aut, fuga id repellat cum soluta iste doloribus labore explicabo similique.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo deleniti laborum labore? Consectetur sunt ipsa eligendi quos, ratione enim aut, fuga id repellat cum soluta iste doloribus labore explicabo similique.</p>
+    </div>
+  )
+}
