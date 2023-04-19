4505ba6cc51c000728d7dd96801e92104fffd690
Jenny Patel <pateljenny555@gmail.com>
2023-04-18 19:16:33 -0700

    Creating Layout Component

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>
-        <h2>404</h2>
-        <p>Sorry, Requested page does not exists.</p>
-    </div>
+    <Layout>
+      <div>
+          <h2>404</h2>
+          <p>Sorry, Requested page does not exists.</p>
+      </div>
+    </Layout>
   )
 }
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      <h1>About</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo deleniti laborum labore? Consectetur sunt ipsa eligendi quos, ratione enim aut, fuga id repellat cum soluta iste doloribus labore explicabo similique.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo deleniti laborum labore? Consectetur sunt ipsa eligendi quos, ratione enim aut, fuga id repellat cum soluta iste doloribus labore explicabo similique.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo deleniti laborum labore? Consectetur sunt ipsa eligendi quos, ratione enim aut, fuga id repellat cum soluta iste doloribus labore explicabo similique.</p>
-    </div>
+    <Layout>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo deleniti laborum labore? Consectetur sunt ipsa eligendi quos, ratione enim aut, fuga id repellat cum soluta iste doloribus labore explicabo similique.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo deleniti laborum labore? Consectetur sunt ipsa eligendi quos, ratione enim aut, fuga id repellat cum soluta iste doloribus labore explicabo similique.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo deleniti laborum labore? Consectetur sunt ipsa eligendi quos, ratione enim aut, fuga id repellat cum soluta iste doloribus labore explicabo similique.</p>
+      </div>
+    </Layout>
   )
 }
