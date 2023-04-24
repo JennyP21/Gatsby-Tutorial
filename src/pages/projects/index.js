62fdb826794ac08781b13ed8783c815921313516
Jenny Patel <pateljenny555@gmail.com>
2023-04-23 22:11:24 -0700

    Adding Projects and Styles

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,40 @@
-import React from 'react'
-import Layout from '../../components/Layout'
-import * as styles from "../../styles/projects.module.css"
+import React from "react";
+import Layout from "../../components/Layout";
+import * as styles from "../../styles/projects.module.css";
+import { Link, graphql } from "gatsby";
 
-export default function Projects() {
+export default function Projects({ data }) {
+  console.log(data);
+  const projects = data.allMarkdownRemark.nodes;
   return (
     <Layout>
       <div className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've Created</h3>
+        <div className={styles.projects}>
+          {projects.map((project) => (
+            <Link to={`/projects/${project.frontmatter.slug}`} key={project.id}>
+              <h3>{project.frontmatter.title}</h3>
+              <p>{project.frontmatter.stack}</p>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
-  )
+  );
 }
+
+export const query = graphql`
+  query ProjectsQuery {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          title
+          stack
+          slug
+        }
+        id
+      }
+    }
+  }
+`;
